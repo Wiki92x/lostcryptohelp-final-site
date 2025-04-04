f25f8616c57424f1fb0cae485fb4d519d6e14cd2
Wiki92x <gemissnasassben@gmail.com>
2025-04-04 11:50:25 +0400

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,13 +1,20 @@
-import ThemeToggle from './ThemeToggle';
+// components/Navbar.jsx
+import Link from 'next/link';
+import Image from 'next/image';
+import logo from '../public/logo-dark.png'; // adjust path/filename
 
 export default function Navbar() {
   return (
-    <nav className="flex justify-between items-center p-4 bg-gray-800 text-white dark:bg-white dark:text-black">
-      <span className="font-bold text-lg">LostCryptoHelp</span>
-      <div className="flex items-center space-x-4">
-        <a href="/deep-scan">Deep Scan</a>
-        <a href="/report">Submit Report</a>
-        <ThemeToggle />
+    <nav className="flex items-center justify-between px-6 py-4 bg-gray-900 text-white dark:bg-white dark:text-black">
+      <Link href="/">
+        <a className="flex items-center space-x-2">
+          <Image src={logo} alt="LostCryptoHelp Logo" width={32} height={32} />
+          <span className="font-bold text-lg">LostCryptoHelp</span>
+        </a>
+      </Link>
+      <div className="space-x-4">
+        <Link href="/deep-scan"><a className="hover:underline">Deep Scan</a></Link>
+        <Link href="/report"><a className="hover:underline">Submit Report</a></Link>
       </div>
     </nav>
   );
