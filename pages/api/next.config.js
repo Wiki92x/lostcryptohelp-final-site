76e22fe53ad010fe53fac711d69e4accdce9cc3e
Wiki92x <gemissnasassben@gmail.com>
2025-04-04 14:01:16 +0400

    Update next.config.js

--- pages/api/next.config.js
+++ pages/api/next.config.js
@@ -1,8 +1,21 @@
-// next.config.js
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  output: 'standalone', // <-- important
+  images: {
+    domains: ['res.cloudinary.com', 'avatars.githubusercontent.com'],
+  },
+  output: 'standalone', // Crucial for Netlify or Vercel
+  env: {
+    BSCSCAN_API_KEY: process.env.BSCSCAN_API_KEY,
+    ETHERSCAN_API_KEY: process.env.ETHERSCAN_API_KEY,
+    TRONSCAN_API_KEY: process.env.TRONSCAN_API_KEY,
+    VERIFY_BNB_ADDRESS: process.env.VERIFY_BNB_ADDRESS,
+    VERIFY_ETH_ADDRESS: process.env.VERIFY_ETH_ADDRESS,
+    VERIFY_TRON_ADDRESS: process.env.VERIFY_TRON_ADDRESS,
+    VITE_TELEGRAM_BOT_TOKEN: process.env.VITE_TELEGRAM_BOT_TOKEN,
+    VITE_TELEGRAM_CHAT_ID: process.env.VITE_TELEGRAM_CHAT_ID,
+  },
 };
 
 module.exports = nextConfig;
